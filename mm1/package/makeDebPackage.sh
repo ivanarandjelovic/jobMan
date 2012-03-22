5df8b0b1fd6a986a9aa31c3612c0e96d51758b70
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2012-03-22 23:49:12 +0100

    Finally, packaging done and works (maintainter field was a problem, had to be enclosed with quotes)

--- mm1/package/makeDebPackage.sh
+++ mm1/package/makeDebPackage.sh
@@ -14,13 +14,22 @@ cp ../resource/com.ubuntu.pkexec.jobMan.policy ./debian/usr/share/polkit-1/actio
 mkdir -p ./debian/usr/share/applications
 cp ../resource/jobMan.desktop ./debian/usr/share/applications
 
-#Prepare controll file (control)
 
+
+#Prepare controll file (control)
 mkdir -p debian/DEBIAN
 cp control ./debian/DEBIAN
 
+
+
+#Don't forget the debian-binary with the version
+echo "2.0" > ./debian/DEBIAN/debian-binary
+
+
+#Build it now
 dpkg-deb --build debian
 
+#Rename to propper name
 mv debian.deb jobMan_1.1-1_amd64.deb
 
 #Finally, cleanup
