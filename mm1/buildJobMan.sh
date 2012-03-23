7fa22e118f76cc93dae86419e68540ef33dfbfaa
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2012-03-23 21:24:25 +0100

    Reverted -m64 flag

--- mm1/buildJobMan.sh
+++ mm1/buildJobMan.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -o jobMan -m64 src/mm1.cpp src/mmWindow.cpp src/job.cpp src/services.cpp `pkg-config --libs --cflags glib-2.0 glibmm-2.4 giomm-2.4 gtkmm-3.0 gconfmm-2.6`
+g++ -o jobMan src/mm1.cpp src/mmWindow.cpp src/job.cpp src/services.cpp `pkg-config --libs --cflags glib-2.0 glibmm-2.4 giomm-2.4 gtkmm-3.0 gconfmm-2.6`
