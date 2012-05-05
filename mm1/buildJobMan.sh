2f017e194c23d4fa6ab5f6d827c9372bdb1afd72
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2012-05-05 22:20:06 +0200

    Minor change in build process

--- mm1/buildJobMan.sh
+++ mm1/buildJobMan.sh
@@ -2,4 +2,8 @@
 
 set -x verbose #echo on
 
-g++ -s -o jobMan src/mm1.cpp src/mmWindow.cpp src/job.cpp src/services.cpp `pkg-config --libs --cflags glib-2.0 glibmm-2.4 giomm-2.4 gtkmm-3.0 gconfmm-2.6`
+#Don't use direct compilation
+#g++ -s -o jobMan src/mm1.cpp src/mmWindow.cpp src/job.cpp src/services.cpp `pkg-config --libs --cflags glib-2.0 glibmm-2.4 giomm-2.4 gtkmm-3.0 gconfmm-2.6`
+
+#Use make for building now!
+make clean all
